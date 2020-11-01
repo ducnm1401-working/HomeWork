c695f0382f878aa917975ed9bee9b1203a211d93
ducnm1401-working <73466093+ducnm1401-working@users.noreply.github.com>
2020-11-01 11:53:07 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import math
+
+x,y,z = 0,0,0
+
+x = input("Nhập số x:")
+y = input("Nhập số y:")
+z = input("Nhập số z:")
+
+#F = (x+y+z)/(x^2+y^2+1) - |x-z*cos(y)|
+
+sum_1 = int(x) + int(y) + int(z)
+
+sum_2 = int(x)**2 + int(y)**2 + 1
+
+abs_result = abs(int(x)-int(z)*math.cos(int(y)))
+
+F = (sum_1 / sum_2) - abs_result
+
+print("Giá trị của F = " + str(F))
